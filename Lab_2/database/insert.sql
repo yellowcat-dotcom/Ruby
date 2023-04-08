d5a62eeb93c0c144d1eb0ac94e08ae2287b179d8
yellowcat-dotcom <valentinagoncarenko975@gmail.com>
2023-04-08 08:58:40 +0300

    insert.sql

---
+++ Lab_2/database/insert.sql
@@ -0,0 +1,4 @@
+insert into students(last_name, first_name, paternal_name, phone, telegram, email, git) values
+('Иванов', 'Иван', 'Иванович', '+79186963312', '@petr_petrodich', 'petya@mail.ru', '@petr_petrodich'),
+('Петров', 'Петр', 'Петрович', '+79183953312', '@ivan', 'ivanivanov@mail.ru', '@ivannn'),
+('Игогок', 'Игог', 'Игогович', '+79186963319', '@IGIG_igigig', 'ivanivanov@mail.ru', '@IGIG_ig');
