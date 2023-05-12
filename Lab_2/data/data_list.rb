12c98732759feade28362ecbccf5d67c3c2f9b54
yellowcat-dotcom <valentinagoncarenko975@gmail.com>
2023-05-12 18:10:05 +0300

    небольшие корректировки

--- Lab_2/data/data_list.rb
+++ Lab_2/data/data_list.rb
@@ -4,8 +4,8 @@ class DataList
   #private_class_method :new
   attr_writer :list
   def initialize(objects)
-    self.sel_objects = []
-    self.list = objects
+    #self.sel_objects = []
+    self.list = objects #self.objects_list
     @observers = []
   end
 
@@ -19,23 +19,30 @@ class DataList
   def notify
     @observers.each { |observer| observer.on_datalist_changed(get_data) }
   end
-  def select(number)
-    #raise ArgumentError, "arg 'number' not Integer" if number.class != Integer
-    sel_objects.append(number)
+  # def select(number)
+  #   #raise ArgumentError, "arg 'number' not Integer" if number.class != Integer
+  #   sel_objects.append(number)
+  # end
+
+  def select_elem(number)
+    self.sel_object = number
   end
 
   # очистить массив выборки
-  def clear_select
-    self.sel_objects = []
-  end
+  # def clear_select
+  #   self.sel_objects = []
+  # end
 
-  def get_selected
-    return [] if sel_objects.empty?
-    list_id=[]
-    sel_objects.each do |i|
-      list_id.append(list[i].id)
-    end
-    list_id
+  # def get_selected
+  #   return [] if sel_objects.empty?
+  #   list_id=[]
+  #   sel_objects.each do |i|
+  #     list_id.append(list[i].id)
+  #   end
+  #   list_id
+  # end
+  def selected_id
+    list[sel_object].id
   end
 
   def get_data
@@ -64,6 +71,6 @@ class DataList
   end
 
   private
-  attr_accessor :sel_objects
+  attr_accessor :sel_object
   attr_reader :list
 end
